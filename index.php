b8492195d0c38597eba6d85b8945ce9309570636
Colin Griffith <tynach2@gmail.com>
2012-06-18 20:23:06 -0700

    Moved stylesheet into a separate file, and cleaned up HTML.

--- index.php
+++ index.php
@@ -5,9 +5,12 @@ $page = new page('templates/main.htmt');
 $page->title = 'Home';
 
 ?>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in velit nibh. Praesent rutrum urna non libero porta nec egestas magna dapibus. Sed vitae diam vel felis molestie viverra. Integer vel quam ut quam convallis pharetra. Suspendisse leo tellus, blandit quis scelerisque nec, viverra dictum magna. Proin turpis turpis, facilisis ut bibendum sit amet, convallis sit amet mi. Sed ut magna velit, vel lobortis lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in velit nibh. Praesent rutrum urna non libero porta nec egestas magna dapibus. Sed vitae diam vel felis molestie viverra. Integer vel quam ut quam convallis pharetra. Suspendisse leo tellus, blandit quis scelerisque nec, viverra dictum magna. Proin turpis turpis, facilisis ut bibendum sit amet, convallis sit amet mi. Sed ut magna velit, vel lobortis lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
 </p>
-<p>This is basically the content of your page. Other PHP stuff can go in here - the title set above can be set anywhere within this page (even the bottom of it), and it'll still work. The lorem ipsum stuff above? Dummy text. Gotta love it.
+<p>
+	This is basically the content of your page. Other PHP stuff can go in here - the title set above can be set anywhere within this page (even the bottom of it), and it'll still work. The lorem ipsum stuff above? Dummy text. Gotta love it.
 </p>
-<p>Click <a href="login.php">here</a> to log in.
+<p>
+	Click <a href="login.php">here</a> to log in.
 </p>
